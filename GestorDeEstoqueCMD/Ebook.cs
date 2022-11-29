6e5fb1f5c133516a8dbd3d3ae569bdcff141a58f
FelipePagnan <luizfelipe130@outlook.com>
2022-11-29 17:43:58 -0300

    Finalizado

---
+++ GestorDeEstoqueCMD/Ebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstoqueCMD
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possivel dar entrada no  estoque de um Ebook, pois é um produto Digital!");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar venda no E-book {nome}");
+            Console.WriteLine("Digite a Qtd. de vendas que você quer consumir: ");
+            int saida = int.Parse(Console.ReadLine());
+            // estoque = estoque + entrada;
+            vendas += saida;
+            Console.WriteLine("Saida registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preco: {preco}");
+            Console.WriteLine($"Vendas: {vendas}");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        }
+    }
+}
